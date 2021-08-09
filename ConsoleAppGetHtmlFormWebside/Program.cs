a5d12c15ae6524a92d4d019bc52ef504484ccc0f
Uncharted <uncha@LAPTOP-1PJR3MM8>
2021-08-09 16:28:02 +0200

    Add project files.

---
+++ ConsoleAppGetHtmlFormWebside/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace ConsoleAppGetHtmlFormWebside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webClient = new WebClient();
+
+            var htmlResponse = webClient.DownloadString("https://www.power.dk/search/?q=computer");
+
+
+            Console.ReadKey();
+        }
+    }
+}
